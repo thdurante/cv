a0619ef5f82f2052ac4540741a46cdda4e5b1d57
Thiago Durante <thdurante@gmail.com>
2024-07-23 16:25:14 +0200

    Updates

---
+++ assets/hello_controller-BQvsIjd6.js
@@ -0,0 +1,1 @@
+import{C as o}from"./index-DyFuTzKd.js";class n extends o{connect(){console.log("Hello controller connected")}}export{n as default};
--- assets/hello_controller-DSVXJzc7.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./index-Cdm_-deN.js";class n extends o{connect(){console.log("Hello controller connected")}}export{n as default};
